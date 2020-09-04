3471548c932f3e43981f7d84c06516ba9e4b4dfd
Joshua Sawyer <joshua@jcsawyer.me>
2020-09-05 00:56:59 +0100

    Add Prepared Statement - Insert Teacher

---
+++ documents/db/PreparedStatements.sql
@@ -0,0 +1,6 @@
+-----------------------------------
+-- Inserts
+-----------------------------------
+
+-- MAKE TEACHER
+INSERT INTO teacher (discord_id, first_name, last_name, title) values(?, ?, ?, ?);
