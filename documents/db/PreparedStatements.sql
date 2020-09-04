71ac28a84ddc93872e06502ba38e98928f988a2e
Joshua Sawyer <joshua@jcsawyer.me>
2020-09-05 00:58:02 +0100

    Add Prepared Statement - Insert Student

--- documents/db/PreparedStatements.sql
+++ documents/db/PreparedStatements.sql
@@ -7,3 +7,6 @@ INSERT INTO teacher (discord_id, first_name, last_name, title) values(?, ?, ?, ?
 
 -- COURSE CREATE
 INSERT INTO course (short_name, full_name) values (?, ?);
+
+-- REGISTER STUDENT / FORCE REGISTER
+INSERT INTO student (discord_id, first_name, last_name) VALUES (?, ?, ?);
