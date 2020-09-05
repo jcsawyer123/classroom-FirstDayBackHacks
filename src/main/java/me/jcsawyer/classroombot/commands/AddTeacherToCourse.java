6af20d584d9de09e9d17b2ccb75583ea870fcf63
Joshua Sawyer <joshua@jcsawyer.me>
2020-09-05 18:38:57 +0100

    Add basic checks to AddTeacherToCourse

--- src/main/java/me/jcsawyer/classroombot/commands/AddTeacherToCourse.java
+++ src/main/java/me/jcsawyer/classroombot/commands/AddTeacherToCourse.java
@@ -7,13 +7,30 @@ public class AddTeacherToCourse extends Command {
 
     private static String[] commands = {"AddTeacherToCourse", "TeacherAdd", "AddTeacher"};
 
+
+    // Usage: !teacherAdd @teacher courseCode
     public AddTeacherToCourse() {
         super("Add Teacher To Course", "Adds teacher to course", "`!AddTeacherToCourse`, `!TeacherAdd`, `!AddTeacher`", commands, "admin");
-        this.requiredUserPermissions = new Permission[]{Permission.ADMINISTRATOR}; // User must have administrator privileges
     }
 
     @Override
     public void handleCommand(CommandEvent event) {
+
+        // Check to see if correct number of args
+        if(event.getArgs().length < 2){
+            event.reply("Please specify: Target and Course Code ");
+            return;
+        }
+        // Check if Admin
+        if(!event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+            event.reply("You must me an Admin to run this command.");
+            event.reactFailure();
+            return;
+        }
+
+        // TODO: Add record to 'teachers_of_course'
+            // Teacher ID
+            // Course ID
 
     }
 }
