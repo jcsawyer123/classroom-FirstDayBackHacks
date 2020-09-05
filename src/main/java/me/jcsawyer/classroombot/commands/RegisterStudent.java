9be263fa58df4ea3c5cbc443696710f3b6a540bf
Joshua Sawyer <joshua@jcsawyer.me>
2020-09-05 16:29:07 +0100

    Create skeleton code for RegisterStudent

---
+++ src/main/java/me/jcsawyer/classroombot/commands/RegisterStudent.java
@@ -0,0 +1,24 @@
+package me.jcsawyer.classroombot.commands;
+
+import me.jcsawyer.classroombot.CommandEvent;
+import net.dv8tion.jda.api.Permission;
+
+public class RegisterStudent extends Command {
+
+    static String[] commands = {"registerStudent", "Register"};
+
+    public RegisterStudent() {
+        super("Register Student", "Register Student.", "`!register`, `!registerStudent`", commands, "student");
+    }
+
+    @Override
+    public void handleCommand(CommandEvent event) {
+    }
+
+    @Override
+    public boolean requireGuild() {
+        return true;
+    }
+
+
+}
