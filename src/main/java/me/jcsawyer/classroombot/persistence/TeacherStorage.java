7b3b43cfde46476471944d7d6324161e47dc8d72
Joshua Sawyer <joshua@jcsawyer.me>
2020-09-06 01:37:53 +0100

    Implement - AddTeacher

--- src/main/java/me/jcsawyer/classroombot/persistence/TeacherStorage.java
+++ src/main/java/me/jcsawyer/classroombot/persistence/TeacherStorage.java
@@ -1,8 +1,13 @@
 package me.jcsawyer.classroombot.persistence;
 
 import com.zaxxer.hikari.HikariDataSource;
+import me.jcsawyer.classroombot.entities.Teacher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class TeacherStorage {
 
@@ -27,4 +32,22 @@ public class TeacherStorage {
     // Methods
     // ----------------
 
+    public void addTeacher(Teacher teacher){
+        try (Connection connection = source.getConnection();
+             PreparedStatement addTeacher = connection.prepareStatement(NEW_TEACHER, PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+
+            // Set Values in Statement
+            addTeacher.setLong(1, teacher.getDiscordId());
+            addTeacher.setString(2, teacher.getFirstname());
+            addTeacher.setString(3, teacher.getLastname());
+            addTeacher.setString(4, teacher.getTitle());
+
+            // Execute Statement
+            addTeacher.executeUpdate();
+
+        } catch (SQLException ex){
+            logger.warn("Failed to add teacher", ex);
+        }
+    }
 }
