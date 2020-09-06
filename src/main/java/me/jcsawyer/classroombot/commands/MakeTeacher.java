32517890ac1837c209aa05967ef39437bb419b0c
AreYouW <zfac022@live.rhul.ac.uk>
2020-09-06 10:41:32 +0100

    Logic of Make teacher command

--- src/main/java/me/jcsawyer/classroombot/commands/MakeTeacher.java
+++ src/main/java/me/jcsawyer/classroombot/commands/MakeTeacher.java
@@ -1,12 +1,15 @@
 package me.jcsawyer.classroombot.commands;
 
+import com.sun.org.apache.bcel.internal.generic.DADD;
 import me.jcsawyer.classroombot.CommandEvent;
-import me.jcsawyer.classroombot.commands.Command;
+import me.jcsawyer.classroombot.persistence.Database;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
 
-public class MakeTeacher extends Command{
+import java.util.concurrent.atomic.AtomicReference;
+
+public class MakeTeacher extends Command {
 
     static String[] commands = {"maketeacher", "createteacher", "setteacher"};
 
@@ -19,12 +22,12 @@ public class MakeTeacher extends Command{
     public void handleCommand(CommandEvent event) {
 
         // Check to see if correct number of args
-        if(event.getArgs().length < 4){
+        if (event.getArgs().length < 4) {
             event.reply("Please specify: Target ID, Title, First Name and Last Name");
             return;
         }
         // Check if Admin
-        if(!event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+        if (!event.getMember().hasPermission(Permission.ADMINISTRATOR)) {
             event.reply("You must me an Admin to run this command.");
             event.reactFailure();
             return;
@@ -39,7 +42,7 @@ public class MakeTeacher extends Command{
 
         // Check if valid UID
         long uid;
-        try{
+        try {
             uid = Long.parseLong(userID);
         } catch (NumberFormatException err) {
             event.reply("Invalid ID");
@@ -47,16 +50,27 @@ public class MakeTeacher extends Command{
         }
 
         // - Add To DB
-            // Check they are not already registered
-            // Discord ID, Title, First Name, Last Name
+        // Check they are not already registered
+        // Discord ID, Title, First Name, Last Name
 
         // - Add "Student" role to user
-        Role teacherRole = null; // TODO: Get from DB
+
+        AtomicReference<Role> teacherRole = new AtomicReference<>(event.getGuild().getPublicRole());
+
+        Database.GUILD_DATA.fetchData(event.getGuild().getIdLong(), (data) -> {
+            teacherRole.set(event.getGuild().getRoleById(data.getTeacherRoleId()));
+        }, (SQLException) -> {
+            System.err.println("error getting data from guild");
+        }); // TODO: Get from DB
         Member member = event.getGuild().getMemberById(uid);
 
+        // Add teacher to db
+        Database.TEACHER_STORAGE.addTeacher(uid,firstName,lastName,title);
 
+
+        assert member != null;
         event.getGuild()
-                .addRoleToMember(member, teacherRole)
+                .addRoleToMember(member, teacherRole.get())
                 .reason("Registered As Teacher by: "
                         + event.getMember().getUser().getAsTag());
         event.reactSuccess();
